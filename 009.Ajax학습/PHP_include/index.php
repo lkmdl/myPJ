98af466d5c33c2a9e637a412f6282f67b91d086e
lkmdl <lkmdl2021@gmail.com>
2021-12-22 17:48:30 +0900

    add

--- 009.Ajax학습/PHP_include/index.php
+++ 009.Ajax학습/PHP_include/index.php
@@ -1,79 +1,113 @@
 <!DOCTYPE html>
 <html lang="ko">
-
 <head>
     <meta charset="UTF-8">
-    <title>PHP 인클루드 연습</title>
-    <style>
-        .box{
-            position: relative;
-            width: 1000px;
-            min-height: 100px;
-            margin: 0 auto;
-            border: 1px solid #ccc;
-        }
-        .top img{
-            height: 100px;
-        }
-        .gnb{
-            position: absolute;
-            right: 10px;
-            bottom: 10px;
-        }
-        .cont{
-            height: 400px;
-            margin-top: 10px;
-            background: url(images/gong.jpg) no-repeat 0/cover;
-        }
-        .info{
-            height: 200px;
-            margin-top: 10px;
-        }
-        .info address{
-            font-style: normal;
-            font-size: 12px;
-            line-height: 28px;
-            padding: 50px;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>보그 코리아에 오신것을 환영합니다!</title>
+    <link rel="shortcut icon" href="images/icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fontisto@v3.0.4/css/fontisto/fontisto.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/media.css">
+    <!-- 미디어쿼리를 본css하단에 link방식으로 넣어준다! -->
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/linksys.js"></script>
+    <script src="js/common.js"></script>
 </head>
-
 <body>
-   <?php 
-    /*
-        [ 외부파일을 php에 삽입하기 ]
-        1. include : 예) <?php include "경로/파일명" ?>
-        2. require : 예) <?php require "경로/파일명" ?>
-        
-        - 위의 두 가지 방법은 html소스 파일에 특정소스를 특정위치에
-        삽입하는 php메서드다!
-        
-        [ include 와 require 의 차이점은? ]
-        - 특정파일 호출시 에러가 나면 해당되는 부분만 에러나고 나머지는
-        그대로 출력하는 것은 include이다!
-        require는 하나만 에러나면 그 하단 소스는 모두 출력하지 않는다!
-        
-        [ include 방식의 장점은? ]
-        - 소스를 하나만 사용하여 관리하므로 유지보수가 쉽다!
-        - 반복소스를 줄여주어 코딩량이 감소된다!
-        
-        [ include 시 사용할 수 있는 파일종류는? ]
-        - 대표적으로는 .inc파일을 사용(인클루드 전용파일)
-        - 그밖에 html, php 파일도 불러올 수 있다!
-    */
-    ?>
-   
-   
-    <!--1.탑영역-->
+
+    <!-- 1. 상단영역 -->
     <?php include "inc/top.inc" ?>
-    <!--2.본문영역-->
-    <section class="box cont"></section>
-    <!--3.하단영역-->
+    <!-- 2. 메인영역 -->
+    <div id="cont" class="bgc">
+        <main class="cont ibx">
+            <!-- 컨텐츠1 - 파트1:1개 -->
+            <section class="pt1 rbx">
+                <div class="rbxIn">
+                    <div class="cbx bgi bg1">
+                        <h2>복싱과 맺은 모델 수주의 이야기</h2>
+                    </div>
+                </div>
+            </section>            
+            <!-- 컨텐츠2 - 파트2:3개 -->
+            <section class="pt2 rbx">
+                <div class="rbxIn fbx">
+                    <div class="cbx bgi bg2">
+                        <h2>안현모,홍지민,강주은 써마지 더 리얼 인터뷰</h2>
+                    </div>
+                    <div class="cbx bgi bg3">
+                        <h2>손 번쩍! 잘 때 ‘만세 자세’가 해로운 이유</h2>
+                    </div>
+                    <div class="cbx bgi bg4">
+                        <h2>아미(Ami)와
+                            장폴구드(Jean-Paul Goude)의
+                            컬래버레이션</h2>
+                    </div>
+                </div>
+            </section>
+            <!-- 컨텐츠3 - 파트1:1개 -->
+            <section class="pt1 rbx">
+                <div class="rbxIn">
+                    <div class="cbx bgi bg5">
+                        <h2>&lt;퀸스 갬빗&gt; 이후 엄청난 신작을 준비 중인 안야 테일러 조이</h2>
+                    </div>
+                </div>
+            </section>            
+            <!-- 컨텐츠4 - 파트2:3개 -->
+            <section class="pt2 rbx">
+                <div class="rbxIn fbx">
+                    <div class="cbx bgi bg6">
+                        <h2>마리아 그라치아 치우리의 디올 크루즈 2022 컬렉션 쇼</h2>
+                    </div>
+                    <div class="cbx bgi bg7">
+                        <h2>약, 그렇게 버리면 되돌아옵니다</h2>
+                    </div>
+                    <div class="cbx bgi bg8">
+                        <h2>셀럽들이 하는 여름 주얼리</h2>
+                    </div>
+                </div>
+            </section>
+            <!-- 컨텐츠5 - 파트1:1개 -->
+            <section class="pt1 rbx">
+                <div class="rbxIn">
+                    <div class="cbx bgi bg9">
+                        <h2>김여진이 감행하는 변화에 대하여</h2>
+                    </div>
+                </div>
+            </section>            
+            <!-- 컨텐츠6 - 파트2:3개 -->
+            <section class="pt2 rbx">
+                <div class="rbxIn fbx">
+                    <div class="cbx bgi bg10">
+                        <h2>모델 아이린이 선택한 여름 원피스 Best</h2>
+                    </div>
+                    <div class="cbx bgi bg11">
+                        <h2>김여진이 감행하는 변화에 대하여</h2>
+                    </div>
+                    <div class="cbx bgi bg12">
+                        <h2>샛노란 ‘자무 주스’가 뜬다!</h2>
+                    </div>
+                </div>
+            </section>            
+            <!-- 컨텐츠7 - 파트2:3개 -->
+            <section class="pt2 rbx">
+                <div class="rbxIn fbx">
+                    <div class="cbx bgi bg13">
+                        <h2>체커보드의 색다른 매력</h2>
+                    </div>
+                    <div class="cbx bgi bg14">
+                        <h2>‘손꾸’의 끝, 핑거 타투와 네일의 조합</h2>
+                    </div>
+                    <div class="cbx bgi bg15">
+                        <h2>지속 가능한 변화를 이끄는 백 브랜드 6</h2>
+                    </div>
+                </div>
+                <h2></h2>
+            </section>
+        </main>
+    </div>
+    <!-- 3. 하단영역 -->
     <?php include "inc/info.inc" ?>
     
-
-
-
 </body>
-
 </html>
